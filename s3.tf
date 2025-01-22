9f65e73ac5e2606b357c9f9cb209a91b8109095c
krishnarajuy <krishnaraju6302@gmail.com>
2025-01-22 16:16:13 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,6 +1,6 @@
 # Create an S3 Bucket
 resource "aws_s3_bucket" "example_bucket" {
-  bucket = "new-s3_bucket-terraform"
+  bucket = "krishna-terraform-s3-bucket"
 
   # Enable versioning
   versioning {
